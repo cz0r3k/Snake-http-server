2310c55cdedf4565861343c17fb3939e15277c25
Piotr Wieczorek <czorek@protonmail.com>
2023-03-02 15:01:05 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build().mount("/", routes![index]).launch().await.expect("TODO: panic message");
+}
